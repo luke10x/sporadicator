10889e32d9f80180f1cb37b2615a220d4bc444f4
Lukas Normantas <spam@discoverops.com>
2018-10-03 21:33:38 +0100

    Listens on two connectors

--- src/main/java/com/discoverops/sporadicator/SporadicatorApplication.java
+++ src/main/java/com/discoverops/sporadicator/SporadicatorApplication.java
@@ -1,12 +1,31 @@
 package com.discoverops.sporadicator;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class SporadicatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SporadicatorApplication.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(8181);
+        return connector;
     }
 }
